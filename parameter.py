df45fa8b040f8ed085d3f9693a9fca754d4711aa
Terrific5454 <wangzhehao66@gmail.com>
2021-10-16 23:03:01 +0800

    global param

---
+++ parameter.py
@@ -0,0 +1,9 @@
+class hyper_parameter:
+    dimension=300
+    epochs = 5
+
+class path:
+    MODEL_PATH = './resources/model/'
+    EMBED_PATH ='./resources/embedding/'
+    DATA_PATH = './data/' #训练数据
+    TEXT_PATH = './text/'  #文本
