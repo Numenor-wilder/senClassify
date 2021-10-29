654b3550491c68c187432fcc4ffb30e9d459e43e
Terrific5454 <wangzhehao66@gmail.com>
2021-10-29 22:06:06 +0800

    add some path, hyper_param class

--- parameter.py
+++ parameter.py
@@ -1,9 +1,15 @@
 class hyper_parameter:
-    dimension=300
-    epochs = 5
+    def __init__(self, dimension, epochs, window):
+        self.dimension = dimension
+        self.epochs = epochs
+        self.window = window
 
 class path:
-    MODEL_PATH = './resources/model/'
-    EMBED_PATH ='./resources/embedding/'
-    DATA_PATH = './data/' #训练数据
-    TEXT_PATH = './resources/text/'  #文本
+    EMBED_MODEL_PATH = r'.\resources\model\embedding'
+    ClASS_MODEL_PATH = r'.\resources\model\classifier'
+    TFIDF_MODEL_PATH = r'.\resources\model\tfidf'
+    CORPUS_PATH = r'.\resources\corpus'
+    EMBED_PATH =r'.\resources\embedding'
+    DATA_PATH = r'.\data' # 训练数据
+    TEXT_PATH = r'.\resources\text'  # 文本
+    RAW_PATH = r'.\resources\raw_data'  # 原始数据
