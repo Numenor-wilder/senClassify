b9bd8bb77395211d4ff69c11ac80568b33b60d57
Terrific5454 <wangzhehao66@gmail.com>
2021-10-29 22:05:00 +0800

    clear the classifier work

--- classifier.py
+++ classifier.py
@@ -1,32 +1,33 @@
-import os 
+import logging
+import os
 
-from logger import Logger
 from embedding import Text_embedding
-from parameter import hyper_parameter, path
+from parameter import path
+from parameter import hyper_parameter as hparam
 
 from sklearn.svm import SVC
 from sklearn.metrics import f1_score, precision_score
 from sklearn.model_selection import train_test_split
+from joblib import dump,load
 import numpy as np
 import pandas as pd
 
 
-LABEL_PATH = './data/labeled_data/'
-TRAIN_PATH = './data/training_data/'
-EMBED_PATH = './embedding/'
+LABEL_PATH = r'.\data\labeled_data'
 
-lg = Logger()
+# lg = Logger()
 
 
 class Classifier(object):
-    def __init__(self, etype='fasttext', epath='') -> None:
+    def __init__(self, etype='fasttext') -> None:
         self.mode= etype
-        self.epath = epath
+
 
     def svm_training(self, features, labels):
-        lg.info("SVM分类器训练开始")
+        logging.info("SVM分类器训练开始")
         if np.any(np.isnan(features)) or np.any(np.isnan(labels)):
-            lg.warning("训练数据有空值！！！")
+            logging.warning("训练数据有空值！！！")
+            return
 
         i = 0
         acc = {}
@@ -39,22 +40,24 @@ class Classifier(object):
             recall[col] = 0
             f1[col] = 0
             prec[col] = 0
+
+        clf = SVC(C=1,kernel='rbf',gamma=0.1)
         while i < 10:     
             for col in labels.columns:
-                lg.info(f"第{i+1}次训练")
+                logging.info(f"第{i+1}次训练")
                 x_train, x_test, y_train, y_test = train_test_split(features, labels[col], test_size=0.2)
-                clf = SVC(C=1,kernel='rbf',gamma=0.1)
+
                 clf.fit(x_train, y_train)
                 y_pred = pd.Series(clf.predict(x_test))
 
                 acc[col] += round(clf.score(x_test, y_test),4)
                 # recall[col] += recall_score(y_test, y_pred, pos_label='positive',average='weighted')
-                f1[col] += f1_score(y_test, y_pred, pos_label='positive', average='weighted')
                 prec[col] += precision_score(y_test, y_pred, pos_label='positive', average='weighted')
-                lg.info(f"{col}测试集准确率: {round(clf.score(x_test, y_test),4)}")
-                # lg.info(f"{col}测试集召回率: {recall_score(y_test, y_pred, pos_label='positive', average='weighted')}")
-                lg.info(f"{col}测试集F1值: {round(f1_score(y_test, y_pred, pos_label='positive', average='weighted'),4)}")
-                lg.info(f"{col}测试集precision: {round(precision_score(y_test, y_pred, pos_label='positive', average='weighted'),4)}")
+                f1[col] += f1_score(y_test, y_pred, pos_label='positive', average='weighted')
+                logging.info(f"{col}测试集准确率: {round(clf.score(x_test, y_test),4)}")
+                # logging.info(f"{col}测试集召回率: {recall_score(y_test, y_pred, pos_label='positive', average='weighted')}")
+                logging.info(f"{col}测试集precision: {round(precision_score(y_test, y_pred, pos_label='positive', average='weighted'),4)}")
+                logging.info(f"{col}测试集F1值: {round(f1_score(y_test, y_pred, pos_label='positive', average='weighted'),4)}")
 
             i += 1
 
@@ -69,44 +72,79 @@ class Classifier(object):
             print(f"{col} mean precision:", round(prec[col],4))
 
 
+    # def get_training_data(self, label_path):
+    #     if self.epath:
+    #         features, labels = self.load_training_data(self.epath, label_path, self.mode)
+    #     else:
+    #         lg.warning("缺少文本特征表示！！！")
+    #         features = None 
+
+    #     return features, labels
 
 
 
-    # 获得训练数据
-    def get_training_data(self, filepath=''):
-        if filepath != '':
-            lg.info("载入自定义词向量文件")
-            embed_path = os.path.join(path.EMBED_PATH, self.mode, filepath)
-            embed = pd.read_csv(embed_path).astype({'NO': 'str'}).iloc[:,1:]
+    def load_training_data(self, emb_filepath, label_filepath, mode=""):
+        logging.info("载入特征数据和标签数据...")
+        if not mode:
+            mode = self.mode
+        
+        emb_path = os.path.join(path.EMBED_PATH, mode, emb_filepath)
+        label_path = os.path.join(LABEL_PATH, label_filepath)
+
+        if os.path.exists(emb_path):
+            features = pd.read_csv(emb_path, dtype={'NO': 'str'}).sort_values('NO')
         else:
-            lg.info("载入模型词向量")
-            text_embed = Text_embedding()
-            embed = text_embed.get_embedding(text_embed.model, text_embed.vocab, text_embed.mode)
+            logging.warning("文本特征数据路径不存在！！！")
+            features = None
+
+        if os.path.exists(label_path):
+            labels = pd.read_csv(label_path, dtype={'NO': 'str'}).sort_values('NO')
+        else:
+            logging.warning("标签文件路径不存在！！！")
+            labels = None
+            
+
+        # 当数据不一致的时候，选择其中相同的部分（临时处理）
+        if features is not None and labels is not None:
+            merge = features.merge(labels, how='inner', on='NO')
+            features_ = features.loc[features['NO'].isin(merge['NO'])]
+            labels_ = labels.loc[labels['NO'].isin(merge['NO'])]
+
+            return features_.iloc[:,1:], labels_.iloc[:,1:]
+        else:
+            return features, labels
+                
+
+
+    # 生成训练数据
+    def generate_training_data(self, doc_path="", label_filepath=""):
+        logging.info("生成训练数据...")
+        # params = hparam(300,5,20)
+        if not doc_path:
+            logging.warning("缺少文本特征输入！！！")
+            return
+        if not label_filepath:
+            logging.warning("缺少标签数据！！！")
+            return
+
+        text_embedding = Text_embedding()
+        if text_embedding.model == None:
+            text_embedding.model = text_embedding.embedding_train(vocab=doc_path)
+        embed = text_embedding.get_embedding(doc=doc_path,save=False)
         
-        label_path = os.path.join(LABEL_PATH, 'all_features.csv')
-        label = pd.read_csv(label_path, dtype={'NO': 'str'}).sort_values('NO').iloc[:,1:]
+        label_path = os.path.join(LABEL_PATH, label_filepath)
+        labels = pd.read_csv(label_path, dtype={'NO': 'str'}).sort_values('NO')
+        features = embed
 
-        lg.info("生成训练数据")
-        train = pd.merge(embed, label)
-        train.to_csv(TRAIN_PATH+'training.csv',encoding='utf-8')
+        if features.shape[0] == labels.shape[0]:  # 临时处理
+            return features.iloc[:,1:], labels.iloc[:,1:]
 
-        return train.iloc[:, 1:hyper_parameter.dimension+1], train.iloc[:, hyper_parameter.dimension+1:]
+        else: # 当数据不一致的时候，选择其中相同的部分
+            merge = features.merge(labels, how='inner', on='NO')
+            features_ = features.loc[features['NO'].isin(merge['NO'])]
+            labels_ = labels.loc[labels['NO'].isin(merge['NO'])]
 
-
-
-    # #生成训练数据
-    # def generate_training_data(label_path, embeding=None, embed_path='', mode='ft'):
-    #     if embed_path != '':
-    #         embed = pd.read_csv(embed_path).rename(columns = {'Unnamed: 0': 'NO'}).astype({'NO': 'str'})
-    #     if embeding:
-    #         embed = embeding
-        
-    #     label = pd.read_csv(label_path, dtype={'NO': 'str'}).sort_values('NO').iloc[:,1:]
-
-    #     train = pd.merge(embed, label)
-    #     train.to_csv(TRAIN_PATH+'training.csv',encoding='utf-8')
-
-    #     return train.iloc[:, 1:hyper_parameter.dimension+1], train.iloc[:, hyper_parameter.dimension+1:]
+            return features_.iloc[:,1:], labels_.iloc[:,1:]
 
 
     # 打印数据标签值分布
